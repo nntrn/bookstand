17ab0604c335610fd17fe8f41912b50c71b2ca4b
nntrn <17685332+nntrn@users.noreply.github.com>
2023-09-10 02:18:13 -0500

    Update scripts

--- start.sh
+++ start.sh
@@ -3,8 +3,20 @@
 SCRIPT="$(realpath "$0")"
 DIR=${SCRIPT%/*}
 
-[[ -d docs/_site ]] && rm -rf $DIR/docs/_site
-[[ -d docs/.jekyll-cache ]] && rm -rf $DIR/docs/.jekyll-cache
+if [[ $# -gt 0 ]]; then
+  for i in "${!ARGS[@]}"; do
+    cur="${ARGS[$i]}"
+    case "$cur" in
+    --rebuild) REBUILD_BLOG=1 ;;
+    esac
+  done
+fi
+
+if [[ -n $REBUILD_BLOG ]]; then
+  echo "Rebuilding..."
+  [[ -d docs/_site ]] && rm -rf $DIR/docs/_site
+  [[ -d docs/.jekyll-cache ]] && rm -rf $DIR/docs/.jekyll-cache
+fi
 
 cd $DIR/docs || exit
 bundle exec jekyll serve
