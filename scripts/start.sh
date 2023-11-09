98bcd84f9b0aea7afc583e6ffef86fa29d1338c6
nntrn <17685332+nntrn@users.noreply.github.com>
2023-11-09 05:45:49 -0600

    Fetch store on rebuild

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 
-DIR="$(git rev-parse --show-toplevel)"
-ARGS=("$@")
+set -e
 
-if [[ $# -gt 0 ]]; then
-  for i in "${!ARGS[@]}"; do
-    cur="${ARGS[$i]}"
-    case "$cur" in
-    --rebuild) REBUILD_BLOG=1 ;;
-    esac
-  done
+ARGS=("$@")
+SCRIPT="$(realpath "$0")"
+
+cd "${SCRIPT%/*}"
+
+if [[ -f build.sh ]]; then
+  BUILD_SCRIPT=$PWD/build.sh
 fi
 
-if [[ $REBUILD_BLOG -eq 1 ]]; then
+DIR="$(git rev-parse --show-toplevel)"
+REMOTE_URL=$(git config --local --get remote.origin.url)
+
+get_store_data() {
+  (
+    cd "$(mktemp -d)"
+    git clone --depth 1 -b assets $REMOTE_URL assets &>/dev/null
+    jq -s 'map({id,title,subtitle,author,isbn,genreNames,pageCount,
+      cover: (.artwork|"\(.url|gsub("{w}.*";""))\(200)x\(.height/(.width/200)|ceil)bb.jpg") 
+    })' ./assets/store/*.json
+  )
+}
+
+if grep -q 'rebuild' <<<"${ARGS[@]}"; then
   echo "Removing _site and .jekyll-cache"
   [[ -d $DIR/docs/_site ]] && rm -rf $DIR/docs/_site
   [[ -d $DIR/docs/.jekyll-cache ]] && rm -rf $DIR/docs/.jekyll-cache
+  $BUILD_SCRIPT --all-data-tasks --all-file-tasks --out $DIR/docs
+  get_store_data >$DIR/docs/_data/store.json
 fi
 
 cd $DIR/docs || exit
