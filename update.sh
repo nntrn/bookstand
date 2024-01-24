213a7d3d5205630c98be23a83e8a76bfb7ac0b14
nntrn <17685332+nntrn@users.noreply.github.com>
2024-01-24 00:59:29 -0600

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Push changes to annotations.json
+#   $ cd ~/bin
+#   $ ln -s /path/to/update.sh update-bookstand.sh
+#   $ update-bookstand.sh
+
+SCRIPT=$(realpath $0)
+DIR=${SCRIPT%/*}
+COMMIT_SCRIPT=$DIR/commit.sh update-bookstand.sh
+
+cd $DIR
+open -a Books
+sleep 15
+queryibooks >annotations.json
+
+[[ $(git ls-files -m annotations.json) ]] && $COMMIT_SCRIPT && git push
+
+osascript -e 'quit app "Books"'
