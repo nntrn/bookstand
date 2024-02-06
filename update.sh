8ad854cc91d6a17cbc95a7d7ab3edb5bd66d6136
nntrn <17685332+nntrn@users.noreply.github.com>
2024-02-05 20:30:25 -0600

    Update scripts

--- update.sh
+++ update.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
+#
 # Push changes to annotations.json
-#   $ cd ~/bin
-#   $ ln -s /path/to/update.sh update-bookstand.sh
-#   $ update-bookstand.sh
-
+#
 set -e
 SCRIPT=$(realpath $0)
 DIR=${SCRIPT%/*}
-COMMIT_SCRIPT=$DIR/commit.sh
 
 cd $DIR
 open -a Books
-sleep 15
-queryibooks >annotations.json
-
-[[ $(git ls-files -m annotations.json) ]] && $COMMIT_SCRIPT && git push
-
+echo "Sleeping ....."
+sleep 30
+echo "Quitting Books"
 osascript -e 'quit app "Books"'
+queryibooks >annotations.json
+git status
